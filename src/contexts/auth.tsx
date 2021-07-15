146ce8f4ca4d86ac7b49622eae82d7197dfa1847
MayaraSs <may_stein@hotmail.com>
2021-07-15 10:59:20 -0300

    add context

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,59 +1,70 @@
-import React, { createContext, useState, useContext, useEffect } from 'react';
-import api from '../services/api';
+import React, {
+  createContext,
+  useState,
+  useContext,
+  useEffect,
+  ReactNode,
+} from 'react'
+
+import api from '../services/api'
+
+interface Props {
+  children: ReactNode
+}
 
 interface AuthContextData {
   login: (email: string, password: string) => Promise<void>
-  token: String
-  user: object | null
+  token: string
+  user: Record<string, string> | null | undefined
   signed: boolean
 }
-const AuthContext = createContext<AuthContextData>({} as AuthContextData);
+const AuthContext = createContext<AuthContextData>({} as AuthContextData)
 
-
-export const AuthProvider: React.FC = ({ children }) => {
+export const AuthProvider: React.FC<Props> = ({ children }: Props) => {
   const [token, setToken] = useState<string>('')
-  const [user, setUser] = useState<object | null>(null);
+  const [user, setUser] = useState<Record<string, string> | null>()
 
   const login = async (email: string, password: string): Promise<void> => {
     const response = await api.post('http://localhost:4000/login', {
-      email: email,
-      password: password,});
-    console.log(response)
-    // const user = response.body
+      email,
+      password,
+    })
 
-    setUser(response.data);
+    setUser(response.data)
     setToken(response.data.token)
-    api.defaults.headers.Authorization = `Bearer ${response.data.token}`;
-    
-    sessionStorage.setItem('@App:token', response.data.token);
-    sessionStorage.setItem('@App:user', JSON.stringify(response.data));
+    api.defaults.headers.Authorization = `Bearer ${response.data.token}`
+
+    sessionStorage.setItem('@App:token', response.data.token)
+    sessionStorage.setItem('@App:user', JSON.stringify(response.data))
   }
 
-
   useEffect(() => {
-    const storagedUser = sessionStorage.getItem('@App:user');
-    const storagedToken = sessionStorage.getItem('@App:token');
+    const storagedUser = sessionStorage.getItem('@App:user')
+    const storagedToken = sessionStorage.getItem('@App:token')
 
     if (storagedToken && storagedUser) {
-      setUser(JSON.parse(storagedUser));
-      api.defaults.headers.Authorization = `Bearer ${storagedToken}`;
+      setUser(JSON.parse(storagedUser))
+      api.defaults.headers.Authorization = `Bearer ${storagedToken}`
     }
-  }, []);
+  }, [])
   return (
-    <AuthContext.Provider value={{
-      signed: Boolean(user),
-      login,
-      token,
-      user}}>
+    <AuthContext.Provider
+      value={{
+        signed: Boolean(user),
+        login,
+        token,
+        user,
+      }}
+    >
       {children}
     </AuthContext.Provider>
-  );
- };
-
- export function useAuth() {
-  const context = useContext(AuthContext);
-
-  return context;
+  )
 }
 
-export default AuthContext;
+export function useAuth() {
+  const context = useContext(AuthContext)
+
+  return context
+}
+
+export default AuthContext
--- src/contexts/establishment.tsx
+++ src/contexts/establishment.tsx
@@ -1,61 +1,62 @@
-import React, { createContext, useContext, useState } from 'react';
-import api from '../services/api';
-// import api from '../services/api';
+import React, { createContext, useContext, useState, ReactNode } from 'react'
 
+import api from '../services/api'
+
+interface Props {
+  children: ReactNode
+}
 export interface Establishment {
-  id?: string,
-  name: string,
-  cnpj: string,
+  id?: string
+  name: string
+  cnpj: string
   locale: string
 }
 
 interface EstablishmentContextData {
   getEstablishments: () => Promise<Array<Establishment>>
   establishments: Array<Establishment>
-  add: (establishments:Establishment) => Promise<void>
+  add: (establishments: Establishment) => Promise<void>
   removeEstablishment: (id: string) => Promise<void>
-
 }
-const EstablishmentContext = createContext<EstablishmentContextData>({} as EstablishmentContextData);
+const EstablishmentContext = createContext<EstablishmentContextData>(
+  {} as EstablishmentContextData
+)
 
+export const EstablishmentProvider: React.FC<Props> = ({ children }: Props) => {
+  const [establishments, setEstablishments] = useState<Array<Establishment>>([])
 
-export const EstablishmentProvider: React.FC = ({ children }) => {
-  const [establishments, setEstablishments] = useState<Array<Establishment>>([]);
-
-  const add = async (
-    establishment: Establishment
-  ): Promise<void> => {
-    await api.post('http://localhost:4000/establishment', establishment);
+  const add = async (establishment: Establishment): Promise<void> => {
+    await api.post('http://localhost:4000/establishment', establishment)
   }
 
-  const removeEstablishment = async(id: string) => {
-    await api.delete(`http://localhost:4000/establishment/${id}`);  
+  const removeEstablishment = async (id: string) => {
+    await api.delete(`http://localhost:4000/establishment/${id}`)
   }
 
-
-  const getEstablishments = async() => {
-    const response = await api.get('http://localhost:4000/establishments');
-    console.log(response.data)
-    if(response.data[0] !== null) setEstablishments([...response.data])
+  const getEstablishments = async () => {
+    const response = await api.get('http://localhost:4000/establishments')
+    if (response.data[0] !== null) setEstablishments([...response.data])
     return response.data
   }
 
-
   return (
-    <EstablishmentContext.Provider value={{
-      add,
-      removeEstablishment,
-      getEstablishments,
-      establishments}}>
+    <EstablishmentContext.Provider
+      value={{
+        add,
+        removeEstablishment,
+        getEstablishments,
+        establishments,
+      }}
+    >
       {children}
     </EstablishmentContext.Provider>
-  );
- };
-
- export function useEstablishment() {
-  const context = useContext(EstablishmentContext);
-
-  return context;
+  )
 }
 
-export default EstablishmentContext;
+export function useEstablishment() {
+  const context = useContext(EstablishmentContext)
+
+  return context
+}
+
+export default EstablishmentContext
